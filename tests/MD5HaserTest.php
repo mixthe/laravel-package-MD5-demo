ece6d36dbdd891b8c50c2ecae58a8581688c26b9
mixthe <kong007sss@gmail.com>
2017-09-04 23:12:35 +0800

    add test

---
+++ tests/MD5HaserTest.php
@@ -0,0 +1,37 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Mixthe\Hasher\MD5Hasher;
+class MD5HaserTest extends TestCase
+{
+    protected $hasher;
+
+    public function setUp()
+    {
+        $this->hasher = new MD5Hasher();
+    }
+
+    public function testMD5HaserTest()
+    {
+        $password = md5('password');
+        $passwordTwo = $this->hasher->make('password');
+
+        $this->assertEquals($password,$passwordTwo);
+    }
+
+    public function testMD5HaserTestWithouSalt()
+    {
+        $password = md5('passwordtest');
+        $passwordTwo = $this->hasher->make('password',['salt'=>'test']);
+
+        $this->assertEquals($password,$passwordTwo);
+    }
+
+    public function testMD5HaserCheck()
+    {
+        $password = md5('password');
+        $passwordCheck = $this->hasher->check('password',$password);
+
+        $this->assertTrue($passwordCheck);
+    }
+}
+
